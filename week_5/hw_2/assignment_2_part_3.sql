0357f206cdb2328a6cffb3fb9af9d17abda260dd
Jay Miranda <132286226+mirjo3@users.noreply.github.com>
2023-06-01 03:36:47 -0700

    Add files via upload

---
+++ week_5/hw_2/assignment_2_part_3.sql
@@ -0,0 +1,19 @@
+--Adding 2 rows of data to customer
+INSERT INTO customer VALUES
+(1001, 'Jim', 'Bean', '123 Main Street', '999-999'),
+(1002, 'Sara', 'Donald', '123 Alphabet Road', '888-8888')
+
+--Adding 2 rows of data to concessions
+INSERT INTO concessions VALUES
+(101, 3.99, 'popcorn', 1),
+(102, .99, 'soda', 1)
+
+--Adding 2 rows of data to tickets
+INSERT INTO tickets VALUES
+(01, '12345601'),
+(02, '12345602')
+
+--Adding 2 rows of data to movie
+INSERT INTO movie VALUES
+(00001, 'Oldboy', 2003, 'thriller'),
+(00002, 'Memento', 2000, 'thriller')
